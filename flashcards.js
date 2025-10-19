450c59ff0fc5f5edf96cdf8b00c40be1bbae7797
Igor Szymczak <igorszymm@gmail.com>
2025-10-19 12:12:05 +0200

    zagadnienia sem1

--- flashcards.js
+++ flashcards.js
@@ -476,7 +476,7 @@ export const flashcards = [
     question: "Na czym polega metoda Linia średnia (LSS)?",
     answer: "Polega na znalezieniu linii, która minimalizuje sumę kwadratów odchyłek punktów pomiarowych od tej linii.",
     explanation: "",
-    hasImage: false,
+    hasImage: true,
     tags: ["dz3", "metody", "zagadnieniaSem2"]
   },
   {
@@ -663,160 +663,438 @@ export const flashcards = [
     hasImage: false,
     tags: ["zagadnieniaSem2"]
   },
-  // {
-  //   id: 84,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 85,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 86,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 87,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 88,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 89,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 90,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 91,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 92,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 93,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 94,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 95,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 96,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 97,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 98,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 99,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 100,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 101,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  // {
-  //   id: 102,
-  //   question: "",
-  //   answer: "",
-  //   explanation: "",
-  //   hasImage: false,
-  //   tags: ["zagadnieniaSem2"]
-  // },
-  
-
+  {
+    id: 84,
+    question: "Podaj definicję pomiaru",
+    answer: "W procesie pomiaru porównujemy wielkość mierzoną z wzorcem tego samego rodzaju co wynika ze wzoru C = A/B, czego wynikiem jest wartość liczbowa C.",
+    explanation: "Krótko: To proces określania wartości wielkości mierzonej poprzez porównanie jej z wartością wzorca.",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 85,
+    question: "Czy do pomiaru potrzebny jest wzorzec?",
+    answer: "Tak",
+    explanation: "Pomiar polega na porównaniu wielkości mierzonej z wzorcem tego samego rodzaju.",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 86,
+    question: "Definicja metrologii",
+    answer: "Nauka o pomiarach, obejmująca metody, narzędzia oraz analizę niepewności pomiarowych.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 87,
+    question: "Definicja wzorca",
+    answer: "fizyczne ujęcie danej jednostki miary",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 88,
+    question: "Jakie warunki musi spełniać wzorzec?",
+    answer: "Musi: <br>- być stabilny w czasie<br>- być łatwy do odtworzenia<br>- być łatwy do stosowania<br>- mieć największą możliwą dokładność",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 89,
+    question: "Warunki zgodności wymiarów wyrobów z jednostkami miar",
+    answer: "Wymiary wyrobów muszą być wyrażone w jednostkach miar zgodnych z układem SI, a pomiary muszą być zgodne z zasadami metrologii.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 90,
+    question: "Jaka jest hierarchia wzorców?",
+    answer: "1) Wzorce międzynarodowe<br>2) Wzorce krajowe<br>3) Wzorce odniesienia<br>4) Wzorce robocze",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 91,
+    question: "Wzorzec podstawowy - definicja",
+    answer: "Wzorzec powszechnie uważany za najbardziej dokładny i stabilny, bez odniesienia do inych wzorców",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 92,
+    question: "Wzorzec odniesienia - definicja",
+    answer: "Wzorzec o najwyższej jakości w danym miejscu, stanowi odniesienie dla wzorców roboczych",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 93,
+    question: "Wzorzec roboczy - definicja",
+    answer: "używany do wzorcowania lub sprawdzania wzorców miar lub przyrządów pomiarowych.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 94,
+    question: "Klasyfikacja narzędzi pomiarowych (według jakich parametrów je dzielimy?)",
+    answer: "- wg rodzaju wielkości mierzonej<br>- wg zasady działania<br>- wg dokładności pomiaru",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 95,
+    question: "Podział wyposażenia pomiarowego",
+    answer: "a) wzorce miar - odtwarzają wartość wielkości<br>b) przyrządy pomiarowe - przetwarzają wielkość mierzoną<br>c) przybory - elementy niezbędne do wykonania pomiaru ale nie oddziałujące na wynik pomiaru",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 96,
+    question: "Klasyfikacja wzorców",
+    answer: "- podstawowe<br>- odniesienia<br>- robocze <br>- specjalne",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 97,
+    question: "Czym jest płytka wzorcowa?",
+    answer: "To stalowa lub ceramiczna płytka o bardzo dokładnie określonych wymiarach i klasie dokładności.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 98,
+    question: "Ile jest powierzchni pomiarowych na płytce wzorcowej?",
+    answer: "Dwie - górna i dolna",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 99,
+    question: "W jaki sposób należy łączyć płytki wzorcowe aby stworzyć stos płytek?",
+    answer: "Wsuwając je jedna w drugą, tak aby powierzchnie pomiarowe stykały się ze sobą. Na zewnątrz powinny znajdować się płytki o największych wymiarach.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 100,
+    question: "Wzorce specjalne - definicja",
+    answer: "Wzorce nietypowe, stosowane do specyficznych pomiarów, np.<br>- śruba mikrometryczna<br>- wzorce zarysów gwintowych<br>- wzorce łuków kołowych",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 101,
+    question: "Definicja suwmiarki",
+    answer: "Przyrząd pomiarowy w którym wzorcem długości jest wzorzec kreskowy, a środkiem zwększającym dokładność odczytu, noniusz. <br>Obecnie są już suwmiarki cyfrowe - bez noniusza.<br>Pomiar do 0.05mm",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 102,
+    question: "Definicja mikrometru",
+    answer: "Przyrząd w którym wzorcem długości jest śruba mikrometryczna o znanym skoku gwintu, a środkiem zwiększającym dokładność odczytu jest bębenek z podziałką i skala bębna (mikropodziałka). <br>Pomiar do 0.01mm",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 103,
+    question: "Jakie są rodzaje czujników?",
+    answer: "- cyfrowy (niezbędny statyw)<br>- analogowy (zegarowy)<br>- uchylny zegarowy i cyfrowy",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 104,
+    question: "Wymień przynajmniej 4 główne metody pomiarowe.",
+    answer: "1. Metoda bezpośrednia<br>2. Metoda bezpośredniego porównania<br>3. Metoda pośrednia<br>4. Metoda różnicowa wychyleniowa<br>5. Metoda koincydencyjna<br>6. Metoda podstawowa",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 105,
+    question: "Na czym polega metoda pomiarowa bezpośrednia?",
+    answer: "Odczyt wartości wielkości mierzonej bezpośrednio z przyrządu pomiarowego.",
+    explanation: "(np. suwmiarka, mikrometr)",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 106,
+    question: "Na czym polega metoda pomiarowa bezpośredniego porównania?",
+    answer: "Porównanie ze wzorcem lub przyrządem wzorcowym.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 107,
+    question: "Na czym polega metoda pomiarowa pośrednia?",
+    answer: "Obliczenie wartości wielkości mierzonej na podstawie innych zmierzonych wielkości.",
+    explanation: "np. pomiar prędkości na podstawie drogi i czasu",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 108,
+    question: "Na czym polega metoda pomiarowa różnicowa wychyleniowa?",
+    answer: "Pomiar małej różnicy między wartością wielkości mierzalnej a znaną wartością tej samej wielkości za pomocą czujnika",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 109,
+    question: "Błąd pomiaru - definicja",
+    answer: "Niezgodność wyniku pomiaru z wartością prawdziwą wielkości mierzonej.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 110,
+    question: "Jak wyraża się błąd pomiaru? Podaj wzór na sposób bezwzględny i względny.",
+    answer: "Bezwzględny: δ = y - y<sub>r</sub> - ilościowa różnica między wynikiem pomiaru y a watością prawdziwą wielkości mierzonej y<sub>r</sub><br>Względny: ε = (δ / y<sub>r</sub>) * 100% - względny stosunek wartości prawdziwej do mierzonej",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 111,
+    question: "Wymień 3 główne rodzaje błędów pomiarowych",
+    answer: "Systematyczne, przypadkowe, nadmierne (grube)",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 112,
+    question: "Czym są błędy systematyczne?",
+    answer: "Ich przyczyny można zidentyfikować i skorygować. Powtarzają się w tych samych warunkach powtarzalności:<br>- Ta sama metoda<br>- Ten sam przyrząd<br>- Ten sam operator<br>- Te same warunki środowiskowe<br>- Krótkie odstępy czasu",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 113,
+    question: "Czym są błędy przypadkowe?",
+    answer: "Wynikają z nieprzewidywalnych i losowych czynników wpływających na pomiar. Nie można ich całkowicie wyeliminować, ale można zminimalizować.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 114,
+    question: "Czym są błędy nadmierne (grube)?",
+    answer: "Wynikają z oczywistych pomyłek operatora lub awarii przyrządu. Wynik obarczony tym błędem należy odrzucić.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 115,
+    question: "Wymień źródła błędów pomiarowych w procesie pomiaru",
+    answer: "- nieścisłość def. wielkości mierzonej<br>- niedokładność wzorca<br>- niedokładność przyrządu pomiarowego<br>- błąd obserwacji<br>- wpływ środowiska<br>- błąd metody<br>- błąd obliczeń",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 116,
+    question: "Niepewność pomiaru - definicja",
+    answer: "Parametr związany z wynikiem pomiaru, charakteryzujący rozrzut wartości, które mogą być przypisane wielkości mierzonej.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 117,
+    question: "Na czym polegają metody szacowania niepewności pomiaru A i B",
+    answer: "A - oparta na statystycznej analizie serii pomiarów<br>B - oparta na analizie innych informacji niż seria pomiarów (np. specyfikacje producenta, dane kalibracyjne)",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 118,
+    question: "Definicja tolerancji wymiarowej",
+    answer: "To dopuszczalny zakres zmienności wymiaru. To różnica między wymiarem maksymalnym (ES) a minimalnym (ei)",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 119,
+    question: "Czy tolerancja może być ujemna?",
+    answer: "Nie",
+    explanation: "Tolerancja jest zawsze wartością dodatnią, ponieważ reprezentuje zakres dopuszczalnych odchyleń od wymiaru nominalnego.",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 120,
+    question: "Definicja odchyłki podstawowej",
+    answer: "To jedna z odchyłek granicznych (ES lub ei) wykorzystywana do określenia pola tolerancji względem linii zerowej.",
+    explanation: "To ta odchyłka której wartość jest bliżej linii zerowej.",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 121,
+    question: "Definicja pasowania",
+    answer: "To wzajemna relacja między dwoma częściami o takim samym wymiarze nominalnym, ale różnych odchyłkach",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 122,
+    question: "Wymień rodzaje pasowań",
+    answer: "a) Luźne<br>b) Ciasne<br>c) Mieszane (zmienne w różnych warunkach)",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 123,
+    question: "Opis symbolowy tolerancji pasowania. Jak go odczytać? opowiedz na przykładzie obrazka:",
+    answer: "fi 30 - wymiar nominalny (mm)<br>H7 - otwór rodzaju H, dokładności 7<br>p6 - wałek rodzaju p, dokłądności 6",
+    explanation: "",
+    hasImage: true,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 124,
+    question: "Pasowanie według stałęgo otworu, co to?",
+    answer: "Zasada tworzenia pasowań wg. której rózne luzy i wciski wynikają z połączenia otworu podstawowego z wałkami o różnych polach tolerancji.",
+    explanation: "Zmieniamy wałek",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 125,
+    question: "Pasowanie według stałego wałka, co to?",
+    answer: "Zasada tworzenia pasowań wg. której różne luzy i wciski wynikają z połączenia wałka podstawowego z otworami o różnych polach tolerancji.",
+    explanation: "Zmieniamy otwór",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 126,
+    question: "Definicja przetwornika pomiarowego",
+    answer: "To urządzenie przetwarzające wejściową wielkość pomiarową na wyjściową, stosując ustandaryzowane zasady, metody i prawa, z określoną niepewnością pomiaru",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 127,
+    question: "Pomiar statyczny, co to?",
+    answer: "To taki pomiar, który nie zmienia się w czasie.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 128,
+    question: "Co opisuje statyczna funkcja przetwarzania?",
+    answer: "Opisuje zależność między wejściem a wyjściem przetwornika",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 129,
+    question: "Jakie są parametry opisujące przetwornik w stanie statycznym?",
+    answer: "- czułość statyczna (wzmocnienie)<br>- histereza<br>- zakres pomiarowy<br>- rozdzielczość urządzenia",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 130,
+    question: "Czym jest parametr czułości statycznej przetwornika?",
+    answer: "To granica stosunku przyrostu wielkości wejściowej do wywołującego tę zmianę przyrostu wielkości wyjściowej",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 131,
+    question: "Czym jest parametr histerezy przetwornika?",
+    answer: "Histereza jest wtedy gdy mamy różny sygnał wyjściowy przy tym samym wejściu, ale w zależności od malejącego bądź rosnącego sygnału.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 132,
+    question: "Czym jest zakres pomiarowy przetwornika?",
+    answer: "To zakres sygnału wejściowego, który przetwornik może zmierzyć bezpiecznie.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 133,
+    question: "Czym jest rozdzielczość przetwornika?",
+    answer: "To najmniejsza różnica wskazania urządzenia",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 134,
+    question: "Czym jest liniowość przetwornika?",
+    answer: "To jego 'wiarygodność', czyli to, czy przetwornik zmienia sygnał wyjściowy proporcjonalnie do wejściowego.",
+    explanation: "Błąd nieliniowości występuje w przykładowej sytuacji:<br> 1V -> 100 stopni<br> 0.5V -> 55 stopni",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 135,
+    question: "Wymień dwa rodzaje metod aproksymacji funkcji",
+    answer: "a) Metody linearyzacji<br>b) Metoda najmniejszych kwadratów",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 136,
+    question: "Jakie są metody linearyzacji funkcji?",
+    answer: "- naciągniętej linii (na oko)<br>- średnich<br>- dwóch punktów wg Hellwiga<br>- kolejnych przybliżeń",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  },
+  {
+    id: 137,
+    question: "Na czym polega metoda aproksymacji funkcji - najmniejszych kwadratów?",
+    answer: "Polega na takim doborze funkcji liniowej, aby suma różnic kwadratów kwadratów odchyleń od tej linii była najmniejsza. Prostą tą nazywamy prostą najmniejszych kwadratów.",
+    explanation: "",
+    hasImage: false,
+    tags: ["zagadnieniaSem1"]
+  } 
 ]
 
   // {
